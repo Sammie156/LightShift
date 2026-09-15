3932c86f03128bab45b0890f87f174bea4df7939
Sammie156 <samanwayadatta@gmail.com>
2026-09-15 07:11:17 +0530

    feat: generating depth map

---
+++ experiments/01_depth.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from PIL import Image
+from transformers import pipeline
+
+import numpy as np
+
+# defining the paths
+INPUT_PATH = "data/input/cat_01.jpg"
+OUTPUT_PATH = "data/output/test_cat_depth.png"
+
+# importing the image
+image = Image.open(INPUT_PATH).convert("RGB")
+
+print("Image loaded")
+print("Size: ", image.size)
+
+# Loading depth-anything-small model
+
+print("loading depth model...")
+
+depth_estimator = pipeline(
+    "depth-estimation",
+    model="depth-anything/Depth-Anything-V2-Small-hf"
+)
+
+print("Model loaded")
+
+# Running the model on the image
+
+print("Running depth estimation...")
+
+result = depth_estimator(image)
+
+print("Depth estimation complete")
+
+print("\nResult: ")
+print(result.keys())
+
+# Storing and saving the estimation
+predicted_depth = result["predicted_depth"]
+depth_array = predicted_depth.detach().cpu().numpy()
+depth_image = result["depth"]
+
+depth_image.save(OUTPUT_PATH)
+print("Saved depth map to: ", OUTPUT_PATH)
+
+np.save("data/output/cat_01_depth.npy", depth_array)
